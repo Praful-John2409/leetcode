71a38652dd90fdd192091e68a7e7813ecc187b8d
Praful John <87015502+Praful-John2409@users.noreply.github.com>
2023-04-23 21:52:36 +0530

    Create Assign Cookies.java

---
+++ easy/Assign Cookies.java
@@ -0,0 +1,26 @@
+//Q Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+// Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j].
+// If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. 
+// Your goal is to maximize the number of your content children and output the maximum number.
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int lastC=0,count=0,found=0;
+        for(int i=0;i<g.length;i++){
+            found=0;
+            for(int j=lastC;j<s.length;j++){
+                if(s[j] >= g[i]){
+                    count++;
+                    lastC=j+1;
+                    found=1;
+                    break;
+                }
+            }
+            if(found==0)
+                break;
+        }
+        return count;
+    }
+}
